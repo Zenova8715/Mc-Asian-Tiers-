3cfcc1140a3bfd7facbb17a5291d3c41b806a1fa
Zenova8715 <voidfury1527@gmail.com>
2026-04-08 15:46:16 +0530

    Delete pages/pages/index.js

--- pages/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{color:"white",background:"#121212",height:"100vh",padding:"20px"}}>
-      <h1>🔥 McAsianTiers</h1>
-      <p>Your site is working 🚀</p>
-    </div>
-  );
-}
